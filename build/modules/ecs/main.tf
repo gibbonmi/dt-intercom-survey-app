d6d50f560913fee3a6620d0ab59cc8db471dca41
Michael Gibbons <michael.gibbons@dynatrace.com>
2018-12-23 18:03:51 -0500

    added some changes

--- build/modules/ecs/main.tf
+++ build/modules/ecs/main.tf
@@ -178,9 +178,9 @@ resource "aws_security_group" "ecs_service" {
   }
 
   ingress {
-    from_port = 0
+    from_port = 3002
     protocol = "tcp"
-    to_port = 0
+    to_port = 3002
     cidr_blocks = ["0.0.0.0/0"]
   }
 
